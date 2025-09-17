589f51e512b4855230360b955e0f895e7a9c21a5
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-09-17 10:40:55 -0300

    feat: adicionando endpoint para pegar posição os ônibus

--- backend/src/controllers/linesController.js
+++ backend/src/controllers/linesController.js
@@ -6,22 +6,42 @@ dotenv.config();
 const token = process.env.TOKEN;
 const BASE_URL = process.env.BASE_URL_SP;
 
-export const authenticate = async (req, res) => {
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true
+});
+
+let sessionCookies = "";
+
+export const authenticate = async () => {
   try {
     const response = await axios.post(`${BASE_URL}/Login/Autenticar?token=${token}`);
+    const setCookieHeader = response.headers["set-cookie"];
 
-    return response.data;
-  } catch(err) {
+    if (setCookieHeader) {
+      sessionCookies = setCookieHeader.join("; ");
+
+    }
+
+    return true; 
+  } catch (err) {
     res.status(500).json({ message: "Internal server error", error: err.message });
   }
 };
 
+/* informações importantes de retorno
+[string]ta = Indica o horário universal (UTC) em que a localização foi capturada. Essa informação está no padrão ISO 8601
+[string]lt0 = Letreiro de destino da linha
+[string]lt1 = Letreiro de origem da linha
+*/
 export const getPositions = async (req, res) => {
     try {
-        const sessionToken = await authenticate();
+        await authenticate();
         
-        const response = await axios.get(`${BASE_URL}/Posicao`, {
-            headers: { 'Authorization': `Bearer ${sessionToken}` }
+        const response = await axiosInstance.get(`/Posicao`, {
+          headers: {
+            Cookie: sessionCookies, 
+          },
         });
         res.status(200).json(response.data);
     } catch(err) {
