26c74964aeaa0675bea0ca5d5a15b01b4dcfe181
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-09-17 11:39:45 -0300

    feat: criando model de preferencias

---
+++ backend/src/models/Preferences.js
@@ -0,0 +1,34 @@
+import sequelize from "../database/client.js";
+
+const Preferences = sequelize.define("Preferences", {
+    home: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    work: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    favorites: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        get() {
+            const rawValue = this.getDataValue('favorites');
+            return rawValue ? JSON.parse(rawValue) : [];
+        },
+        set(value) {
+            this.setDataValue('favorites', JSON.stringify(value));
+        }
+    },
+    recents: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        get() {
+            const rawValue = this.getDataValue('recents');
+            return rawValue ? JSON.parse(rawValue) : [];
+        },
+        set(value) {
+            this.setDataValue('recents', JSON.stringify(value));
+        }
+    }
+});
