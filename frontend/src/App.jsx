f0cb65aa69dc8c6257052f1c7464e248e74c2c9f
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-10-29 11:50:27 -0300

    feat: criando funcionalidade para authenticação de rotas e criação de novas rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
 import './App.css'
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem('token');
+};
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated() ? (
+              <Navigate to="/login" replace />
+            ) : (
+              <Navigate to="/register" replace />
+            )
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
